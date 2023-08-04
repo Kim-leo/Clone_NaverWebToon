041be63ea429f5cfb3bac5d7a6053fd12162c3d2
Kim-leo <ochell2@naver.com>
2023-08-04 21:35:14 +0900

    Initial Commit

---
+++ Clone_NaverWebToon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone_NaverWebToon
+//
+//  Created by 김승현 on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
